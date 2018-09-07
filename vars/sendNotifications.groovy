6dec38d753cdb0d6a5053ba1a87d72b1cbcef00a
Juan Chimienti <juan.chimienti@gmail.com>
2018-09-07 11:38:02 -0300

    Removed email & hipchat notifications

--- vars/sendNotifications.groovy
+++ vars/sendNotifications.groovy
@@ -27,12 +27,4 @@ def call(String buildStatus = 'STARTED') {
   // Send notifications
   slackSend (color: colorCode, message: summary)
 
-  hipchatSend (color: color, notify: true, message: summary)
-
-  emailext (
-      to: 'bitwiseman@bitwiseman.com',
-      subject: subject,
-      body: details,
-      recipientProviders: [[$class: 'DevelopersRecipientProvider']]
-    )
 }
